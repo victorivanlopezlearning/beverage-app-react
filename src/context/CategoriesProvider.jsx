b2f40c00295b38408416532deaf10b0ad84f4c39
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-31 16:15:15 -0600

    Add: getCategories

--- src/context/CategoriesProvider.jsx
+++ src/context/CategoriesProvider.jsx
@@ -1,11 +1,31 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const CategoriesContext = createContext();
 
 export const CategoriesProvider = ({ children }) => {
+
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`;
+    try {
+      const { data } = await axios(url);
+      setCategories(data.drinks);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, [])
+
   return (
     <CategoriesContext.Provider
-      value={{}}
+      value={{
+        categories,
+      }}
     >
       {children}
     </CategoriesContext.Provider>
